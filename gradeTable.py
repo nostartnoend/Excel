3159911fdec4be8433bb0371fa00f22376cf23e9
OdericoOfficial <2919128969@qq.com>
2023-11-04 16:10:46 +0800

    no message

---
+++ gradeTable.py
@@ -0,0 +1,16 @@
+import xlrd
+
+# path: excel±íÂ·¾¶ 
+def getDataTable(path):
+    dataBook = xlrd.open_workbook(path);
+    dataSheet = dataBook.sheet_by_name(dataBook.sheet_names()[0])
+    columnName = [ 'name', 'course', 'grade1', 'grade2' ]
+    columnIndex = [ 2, 3, 5, 6 ]    
+    dataTable = []
+    for i in range(2, dataSheet.nrows):
+        dataRow = {}    
+        for j in range(0, 4):
+            value = dataSheet.cell_value(i, columnIndex[j])
+            dataRow[columnName[j]] = value
+        dataTable.append(dataRow)
+    return dataTable
